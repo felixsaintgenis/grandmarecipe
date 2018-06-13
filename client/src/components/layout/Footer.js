76eb35e6a1961626a7c3d5adae4cbf5894ba7bb1
Félix Saint-Genis <saintgenisfelix@gmail.com>
2018-06-13 17:02:10 +0200

    Code the front of the landing page

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="text-white text-center footer">
+      Copyright &copy; {new Date().getFullYear()} GrandMaRemedies
+    </footer>
+  );
+};
