fb9145dba474b75618f44553396acab6a939cadc
Félix Saint-Genis <saintgenisfelix@gmail.com>
2018-06-14 10:45:12 +0200

    add register and login front-end form

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-sm navbar-dark mb-4">
         <div className="container-fluid">
-          <a className="navbar-brand" href="landing.html">
+          <Link className="navbar-brand" to="/">
             GrandMaRemedies
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
             data-toggle="collapse"
             data-target="#mobile-nav"
           >
-            <span className="navbar-toggler-icon" />
+            <span className="navbar-toggler-icon"/>
           </button>
 
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <a className="nav-link" href="profiles.html">
+                <a className="nav-link" href="#">
                   {' '}
                   Find remedies
                 </a>
@@ -29,14 +30,14 @@ class Navbar extends Component {
 
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link" href="register.html">
+                <Link to="/register" className="nav-link" to="/register">
                   Sign Up
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="login.html">
+                <Link to="/Login" className="nav-link">
                   Login
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
