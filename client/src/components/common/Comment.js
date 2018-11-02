4f24f6017cbd571c3227b10c074367a313c6fb20
Félix Saint-Genis <saintgenisfelix@gmail.com>
2018-11-02 11:19:07 +0100

    format date in comments

--- client/src/components/common/Comment.js
+++ client/src/components/common/Comment.js
@@ -5,14 +5,16 @@ const Comment = ({
   username,
   profilePicture,
   date,
-  body
+  body 
+  
 }) => {
+  date =  new Date(date)
   return (
-    <div>
-    <div className="col-md-12">
+<div>
+<div className="col-md-12">
 <div className="panel panel-default">
 <div className="panel-heading">
-<strong>{username}</strong> <span className="text-muted">{date}</span>
+<strong>{username}</strong> <span className="text-muted">{date.toDateString()}</span>
 </div>
 <div className="panel-body">
 {body}
