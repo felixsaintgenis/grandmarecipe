8bd50c3d84ddd410b4367dc515ef611400f2c672
Félix Saint-Genis <saintgenisfelix@gmail.com>
2018-12-07 10:32:40 +0100

    remove passport social strategies

--- api/config/config.js
+++ api/config/config.js
@@ -2,6 +2,5 @@ export default {
   databaseUrl:
     "mongodb://felixsaintgenis:grandmarecipe123@ds211309.mlab.com:11309/grandmarecipe",
   port: 5000,
-  secret: "grandmarecipe",
-  keyfacebook: ""
+  secret: "grandmarecipe"
 };
